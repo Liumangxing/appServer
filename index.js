cd242efaed8e8b44cb5d9736eb5ba7f18932d5c0
jjzhang <403549730@qq.com>
2020-11-05 16:18:54 +0800

    删除无用文件

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var path = require("path");
-var app = express();
-app.use('/', express.static(path.join(__dirname, '..', 'client')));
-app.listen(80);
